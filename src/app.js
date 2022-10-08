ae8e57430cd294cc0fdce63b189bbf8b47e9d3fb
gabytochon <gabrielatochon@gmail.com>
2022-10-08 22:36:57 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let domainGenerator = [];
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "awesome"];
+let noun = ["jogger", "racoon", "whale", "panda"];
+let domain = [".com", ".net"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < domain.length; l++) {
+        domainGenerator.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+      }
+    }
+  }
+}
+console.log(domainGenerator);
